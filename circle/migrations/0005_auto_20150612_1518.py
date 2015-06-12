ad64c5559e8b17d4f3a6f0bdfd5741c9e4241b47
Daniel Zhou (TechBrewery) <danithaca@gmail.com>
2015-06-12 18:01:20 -0400

    added migrations file

---
+++ circle/migrations/0005_auto_20150612_1518.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('circle', '0004_superset'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='circle',
+            name='members',
+            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL, through='circle.Membership'),
+        ),
+        migrations.AlterField(
+            model_name='circle',
+            name='area',
+            field=models.ForeignKey(default=1, to='location.Area'),
+        ),
+        migrations.AlterField(
+            model_name='circle',
+            name='owner',
+            field=models.ForeignKey(related_name='owner', to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AlterUniqueTogether(
+            name='superset',
+            unique_together=set([('child', 'parent')]),
+        ),
+    ]
