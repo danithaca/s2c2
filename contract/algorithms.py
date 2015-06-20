f81b8201d5c725c10e3f3bc781e25fb35405a060
Daniel Zhou (Windows) <danithaca@gmail.com>
2015-06-19 20:30:13 -0400

    update active contract regularly (not working)

--- contract/algorithms.py
+++ contract/algorithms.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from django.db.models import F
 from circle.models import Membership, Circle
 from contract.models import Match
 from puser.models import PUser
@@ -19,9 +20,9 @@ class RecommenderStrategy(metaclass=ABCMeta):
 class RandomFavoriteRecommender(RecommenderStrategy):
     def recommend(self, contract):
         initiate_user = PUser.from_user(contract.initiate_user)
-        initiate_user_circle = initiate_user.get_personal_circle()
+        initiate_user_circle = initiate_user.get_personal_circle(contract.area)
         # order by random and limit by 10.
-        for ms in Membership.objects.filter(circle=initiate_user_circle, active=True, approved=True, type=Circle.Type.PERSONAL.value).order_by('?')[:10]:
+        for ms in Membership.objects.filter(circle=initiate_user_circle, active=True, approved=True, type=Circle.Type.PERSONAL.value).exclude(member=contract.initiate_user).order_by('?')[:10]:
             target_user = ms.member
             score = ms.updated.timestamp()
             match, created = Match.objects.get_or_create(contract=contract, target_user=target_user, defaults={
@@ -44,12 +45,25 @@ class RandomFavoriteRecommender(RecommenderStrategy):
 # recommend more people till we got every target user
 class IncrementalAllRecommender(RecommenderStrategy):
     def recommend(self, contract):
-        all_circle_id = Circle.objects.filter()
+        personal_circle = set(Circle.objects.filter(owner=contract.initiate_user, area=contract.area, type=Circle.Type.PERSONAL.value).values_list('id', flat=True))
+        public_circle = set(Circle.objects.filter(membership__member=contract.initiate_user, area=contract.area, type=Circle.Type.PUBLIC.value).values_list('id', flat=True))
+
+        for membership in Membership.objects.filter(circle__in=(personal_circle | public_circle), active=True, approved=True).exclude(member=contract.initiate_user).exclude(member__match__contract=contract, member__match__circles=F('circle')).order_by('?')[:10]:
+            target_user = membership.member
+            score = membership.updated.timestamp()
+            match, created = Match.objects.get_or_create(contract=contract, target_user=target_user, defaults={
+                'status': Match.Status.INITIALIZED.value,
+                'score': score,
+            })
+            if not created:
+                match.score += score        # increase score if there are more circles.
+            match.circles.add(membership.circle)
 
 
 # todo: other algorithms
 # 1. favorite plus public circle
 # 2. used sitters go first
+# 3. friend's friends.
 
 
 class L1Recommender(RandomFavoriteRecommender):
---
+++ p2/management/commands/update_active_contract.py
@@ -0,0 +1,18 @@
+import logging
+from django.core.management import BaseCommand
+from contract.algorithms import L2Recommender
+from contract.models import Contract, Match
+
+
+class Command(BaseCommand):
+    help = 'Update active contracts and their matches.'
+
+    def handle(self, *args, **options):
+        qs = Contract.objects.filter(status=Contract.Status.ACTIVE.value)
+        logging.info('Total active contracts to handle: %s' % qs.count())
+        recommender = L2Recommender()
+        for contract in qs:
+            recommender.recommend(contract)
+            # if there are new matches not engaged, engage them
+            for match in Match.objects.filter(contract=contract, status=Match.Status.INITIALIZED.value):
+                match.engage()
