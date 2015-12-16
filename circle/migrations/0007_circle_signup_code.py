541b129d7a563f013db20b9c8d4c3d2a390a2e60
knowsun <knowsun@knowsun.com>
2015-12-16 21:42:02 +0000

    update migration

--- circle/migrations/0007_circle_signup_code.py
+++ circle/migrations/0007_circle_signup_code.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('account', '0003_auto_20150902_1827'),
+        ('account', '0001_initial'),
         ('circle', '0006_circle_active'),
     ]
 
